0e28a0c161d353462bc8723d519c946ffc0081df
natruz <88256962+natruz@users.noreply.github.com>
2022-06-25 10:14:33 +0800

    Initial Commit

---
+++ SAP-L13-Todos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SAP-L13-Todos
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 25/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SAP-L13-Todos/SAP_L13_TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAP_L13_TodosApp.swift
+//  SAP-L13-Todos
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SAP_L13_TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
